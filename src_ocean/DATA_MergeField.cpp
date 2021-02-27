8a5abdd20eeff77b49d8e3152986b03ff201a59b
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2021-02-27 10:34:44 +0100

    Add timings.

--- src_ocean/DATA_MergeField.cpp
+++ src_ocean/DATA_MergeField.cpp
@@ -1,6 +1,7 @@
 #include "Model_interpolation.h"
 int main(int argc, char *argv[])
 {
+  std::chrono::time_point<std::chrono::system_clock> time1 = std::chrono::system_clock::now();
   try {
     FullNamelist eFull=NAMELIST_GetStandardMODEL_MERGING();
     if (argc != 2) {
@@ -18,5 +19,6 @@ int main(int argc, char *argv[])
     std::cerr << "Error in DATA_MergeField\n";
     exit(e.eVal);
   }
-
+  std::chrono::time_point<std::chrono::system_clock> time2 = std::chrono::system_clock::now();
+  std::cerr << "runtime = " << std::chrono::duration_cast<std::chrono::seconds>(time2 - time1).count() << "\n";
 }
