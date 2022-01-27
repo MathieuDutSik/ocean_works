2325fc05498e198bbc9d43bffdaa470a7e760d57
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2022-01-27 14:40:02 +0100

    Insert.

---
+++ src_ocean/ROMS_VerticalStratification_Diagnostics.cpp
@@ -0,0 +1,25 @@
+#include "Plotting_fct.h"
+int main(int argc, char *argv[])
+{
+  srand_random_set();
+  std::chrono::time_point<std::chrono::system_clock> time1 = std::chrono::system_clock::now();
+  try {
+    FullNamelist eFull = NAMELIST_ROMS_VERTICAL_STRATIFICATION_DIAGNOSTIC();
+    if (argc != 2) {
+      std::cerr << "ROMS_CreateDefaultSetting [file.nml]\n";
+      std::cerr << "with file.nml the file describing the default variable chosen\n";
+      NAMELIST_WriteNamelistFile(std::cerr, eFull);
+      return -1;
+    }
+    std::string eFileName=argv[1];
+    NAMELIST_ReadNamelistFile(eFileName, eFull);
+    CreateDefaultInputFiles(eFull);
+    std::cerr << "Normal termination of ROMS_CreateDefaultSetting\n";
+  }
+  catch (TerminalException const& e) {
+    std::cerr << "Error in ROMS_CreateDefaultSetting\n";
+    exit(e.eVal);
+  }
+  std::chrono::time_point<std::chrono::system_clock> time2 = std::chrono::system_clock::now();
+  std::cerr << "runtime = " << std::chrono::duration_cast<std::chrono::seconds>(time2 - time1).count() << "\n";
+}
