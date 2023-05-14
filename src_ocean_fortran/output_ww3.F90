f406ee87619f904eec49c944d095a661451e7a11
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2023-05-14 15:23:12 +0200

    Some insert.

---
+++ src_ocean_fortran/output_ww3.F90
@@ -0,0 +1,166 @@
+!**********************************************************************
+!*                                                                    *
+!**********************************************************************
+SUBROUTINE GENERIC_NETCDF_ERROR_WWM(CallFct, idx, iret)
+  USE NETCDF
+  USE DATAPOOL
+  implicit none
+  integer, intent(in) :: iret, idx
+  character(*), intent(in) :: CallFct
+  character(len=500) :: CHRERR
+  IF (iret .NE. nf90_noerr) THEN
+     CHRERR = nf90_strerror(iret)
+     WRITE(wwmerr,*) 'NETCDF error in routine ', TRIM(CallFct), ' Error Message: ', TRIM(CHRERR), ' Position in the routine :', idx
+     CALL WWM_ABORT(wwmerr)
+  ENDIF
+END SUBROUTINE GENERIC_NETCDF_ERROR_WWM
+!**********************************************************************
+!*                                                                    *
+!**********************************************************************
+SUBROUTINE OUTPUT_SPECTRUM
+  USE NETCDF
+  USE DATAPOOL
+  IMPLICIT NONE
+  character(len=400) FILE_NAME
+  DO IB=1,IWBMNP
+     WRITE (FILE_NAME,40) TRIM(PRE_FILE_NAME),IB
+40   FORMAT (a,'_',i4.4,'.nc')
+     !
+     ! Creating the file
+     !
+     iret = nf90_create(TRIM(FileSave), NF90_CLOBBER, ncid)
+     CALL GENERIC_NETCDF_ERROR_WWM(CallFct, 1, iret)
+     !
+     iret = nf90_def_dim(ncid, 'time', 0, time_dims)
+     CALL GENERIC_NETCDF_ERROR_WWM(CallFct, 3, iret)
+     !
+     iret = nf90_def_dim(ncid, 'station', 1, station_dims)
+     CALL GENERIC_NETCDF_ERROR_WWM(CallFct, 3, iret)
+     !
+     iret = nf90_def_dim(ncid, 'string16', 1, string16_dims)
+     CALL GENERIC_NETCDF_ERROR_WWM(CallFct, 3, iret)
+     !
+     iret = nf90_def_dim(ncid, 'frequency', NUMSIG, frequency_dims)
+     CALL GENERIC_NETCDF_ERROR_WWM(CallFct, 3, iret)
+     !
+     iret = nf90_def_dim(ncid, 'direction', NUMDIR, direction_dims)
+     CALL GENERIC_NETCDF_ERROR_WWM(CallFct, 3, iret)
+     !
+     iret=nf90_def_var(ncid,"time",NF90_DOUBLE,(/ time_dims/),var_id)
+     CALL GENERIC_NETCDF_ERROR_WWM(CallFct, 4, iret)
+     iret=nf90_put_att(ncid,var_id,"units","days since 1990-01-01T00:00:00Z")
+     iret=nf90_put_att(ncid,var_id,"long_name","julian day (UT)")
+     iret=nf90_put_att(ncid,var_id,"standard_name","time")
+     iret=nf90_put_att(ncid,var_id,"conventions","Relative julian days with decimal part (as parts of the day)")
+     iret=nf90_put_att(ncid,var_id,"axis","T")
+     !
+     iret=nf90_def_var(ncid,"station",NF90_INT,(/ station_dims/),var_id)
+     CALL GENERIC_NETCDF_ERROR_WWM(CallFct, 4, iret)
+     iret=nf90_put_att(ncid,var_id,"long_name","station id")
+     iret=nf90_put_att(ncid,var_id,"axis","X")
+     !
+     iret=nf90_def_var(ncid,"string16",NF90_INT,(/ string16_dims/),var_id)
+     CALL GENERIC_NETCDF_ERROR_WWM(CallFct, 4, iret)
+     iret=nf90_put_att(ncid,var_id,"long_name","station_name number of characters")
+     iret=nf90_put_att(ncid,var_id,"axis","W")
+     !
+     iret=nf90_def_var(ncid,"station_name",NF90_CHAR,(/ station_dims, string16_dims/),var_id)
+     CALL GENERIC_NETCDF_ERROR_WWM(CallFct, 4, iret)
+     iret=nf90_put_att(ncid,var_id,"long_name","station name")
+     iret=nf90_put_att(ncid,var_id,"content","XW")
+     iret=nf90_put_att(ncid,var_id,"associates","station string16")
+     !
+     iret=nf90_def_var(ncid,"longitude",NF90_REAL,(/ time_dims, station_dims/),var_id)
+     CALL GENERIC_NETCDF_ERROR_WWM(CallFct, 4, iret)
+     iret=nf90_put_att(ncid,var_id,"units","degree_east")
+     iret=nf90_put_att(ncid,var_id,"long_name","longitude")
+     iret=nf90_put_att(ncid,var_id,"standard_name","longitude")
+     iret=nf90_put_att(ncid,var_id,"valid_min",-180)
+     iret=nf90_put_att(ncid,var_id,"valid_max", 180)
+     iret=nf90_put_att(ncid,var_id,"content","TX")
+     iret=nf90_put_att(ncid,var_id,"associates","time station")
+     !
+     iret=nf90_def_var(ncid,"latitude",NF90_REAL,(/ time_dims, station_dims/),var_id)
+     CALL GENERIC_NETCDF_ERROR_WWM(CallFct, 4, iret)
+     iret=nf90_put_att(ncid,var_id,"units","degree_north")
+     iret=nf90_put_att(ncid,var_id,"long_name","latitude")
+     iret=nf90_put_att(ncid,var_id,"standard_name","latitude")
+     iret=nf90_put_att(ncid,var_id,"valid_min",-90)
+     iret=nf90_put_att(ncid,var_id,"valid_max", 90)
+     iret=nf90_put_att(ncid,var_id,"content","TX")
+     iret=nf90_put_att(ncid,var_id,"associates","time station")
+     !
+     iret=nf90_def_var(ncid,"frequency",NF90_REAL,(/ frequency_dims /),var_id)
+     CALL GENERIC_NETCDF_ERROR_WWM(CallFct, 4, iret)
+     iret=nf90_put_att(ncid,var_id,"units","s-1")
+     iret=nf90_put_att(ncid,var_id,"long_name","frequency of center band")
+     iret=nf90_put_att(ncid,var_id,"standard_name","sea_surface_wave_frequency")
+     iret=nf90_put_att(ncid,var_id,"globwave_name","frequency")
+     iret=nf90_put_att(ncid,var_id,"valid_min", 0)
+     iret=nf90_put_att(ncid,var_id,"valid_max",10)
+     iret=nf90_put_att(ncid,var_id,"axis","Y")
+     !
+     iret=nf90_def_var(ncid,"frequency1",NF90_REAL,(/ frequency_dims /),var_id)
+     CALL GENERIC_NETCDF_ERROR_WWM(CallFct, 4, iret)
+     iret=nf90_put_att(ncid,var_id,"units","s-1")
+     iret=nf90_put_att(ncid,var_id,"long_name","frequency of lower band")
+     iret=nf90_put_att(ncid,var_id,"standard_name","frequency_of_lower_band")
+     iret=nf90_put_att(ncid,var_id,"globwave_name","frequency_lower_band")
+     iret=nf90_put_att(ncid,var_id,"valid_min", 0)
+     iret=nf90_put_att(ncid,var_id,"valid_max",10)
+     iret=nf90_put_att(ncid,var_id,"axis","Y")
+     iret=nf90_put_att(ncid,var_id,"associates","frequency")
+     !
+     iret=nf90_def_var(ncid,"frequency2",NF90_REAL,(/ frequency_dims /),var_id)
+     CALL GENERIC_NETCDF_ERROR_WWM(CallFct, 4, iret)
+     iret=nf90_put_att(ncid,var_id,"units","s-1")
+     iret=nf90_put_att(ncid,var_id,"long_name","frequency of upper band")
+     iret=nf90_put_att(ncid,var_id,"standard_name","frequency_of_upper_band")
+     iret=nf90_put_att(ncid,var_id,"globwave_name","frequency_upper_band")
+     iret=nf90_put_att(ncid,var_id,"valid_min", 0)
+     iret=nf90_put_att(ncid,var_id,"valid_max",10)
+     iret=nf90_put_att(ncid,var_id,"axis","Y")
+     iret=nf90_put_att(ncid,var_id,"associates","frequency")
+     !
+     iret=nf90_def_var(ncid,"direction",NF90_REAL,(/ direction_dims /),var_id)
+     CALL GENERIC_NETCDF_ERROR_WWM(CallFct, 4, iret)
+     iret=nf90_put_att(ncid,var_id,"units","degree")
+     iret=nf90_put_att(ncid,var_id,"long_name","sea surface wave to direction")
+     iret=nf90_put_att(ncid,var_id,"standard_name","sea_surface_wave_to_direction")
+     iret=nf90_put_att(ncid,var_id,"globwave_name","direction")
+     iret=nf90_put_att(ncid,var_id,"valid_min", 0)
+     iret=nf90_put_att(ncid,var_id,"valid_max",360)
+     iret=nf90_put_att(ncid,var_id,"axis","Z")
+     !
+     iret=nf90_def_var(ncid,"efth",NF90_REAL,(/ time_dims, station_dims, frequency_dims, direction_dims /),var_id)
+     CALL GENERIC_NETCDF_ERROR_WWM(CallFct, 4, iret)
+     iret=nf90_put_att(ncid,var_id,"units","m2 s rad-1")
+     iret=nf90_put_att(ncid,var_id,"long_name","sea surface wave directional variance spectral density")
+     iret=nf90_put_att(ncid,var_id,"standard_name","sea_surface_wave_directional_variance_spectral_density")
+     iret=nf90_put_att(ncid,var_id,"globwave_name","directional_variance_spectral_density")
+     iret=nf90_put_att(ncid,var_id,"scale_factor", 1.0)
+     iret=nf90_put_att(ncid,var_id,"add_offset", 0.0)
+     iret=nf90_put_att(ncid,var_id,"valid_min", 0)
+     iret=nf90_put_att(ncid,var_id,"valid_max",1.e+20f)
+     iret=nf90_put_att(ncid,var_id,"content","TXYZ")
+     iret=nf90_put_att(ncid,var_id,"associates","time station frequency direction")
+     !
+     iret = nf90_close(ncid)
+     CALL GENERIC_NETCDF_ERROR_WWM(CallFct, 5, iret)
+     !
+     ! Writing the data
+     !
+     iret = nf90_open(TRIM(FileSave), NF90_WRITE, ncid)
+     CALL GENERIC_NETCDF_ERROR_WWM(CallFct, 6, iret)
+     !
+     
+     
+     iret=nf90_inq_varid(ncid, "time", var_id)
+     CALL GENERIC_NETCDF_ERROR_WWM(CallFct, 7, iret)
+     iret=nf90_put_var(ncid,var_id,ListTimeWrite,start=(/1, iIter/), count=(/ np_total, 1 /))
+     CALL GENERIC_NETCDF_ERROR_WWM(CallFct, 8, iret)
+     !
+     iret = nf90_close(ncid)
+     CALL GENERIC_NETCDF_ERROR_WWM(CallFct, 9, iret)
+  END DO
+END SUBROUTINE OUTPUT_SPECTRUM
